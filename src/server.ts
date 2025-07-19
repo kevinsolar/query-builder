37db248b70c1d0b6606f772018a5296d1cb64fbb
Kevin Solar <kevinleitesolar@hotmail.com>
2025-07-19 16:53:50 -0300

    feat: add insert = post, add select = get, add update = put and add delete.
    learn how ot use RAW with knex, to write SQL code to do the actions we need.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,41 @@
 import express, { Request, Response } from "express"
+import { knex } from "./database/knex"
+import { request } from "http"
 
 const app = express()
 app.use(express.json())
 
-app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World!" })
+app.post("/courses", async (request: Request, response: Response) => {
+	const { name } = request.body
+
+	await knex("courses").insert({ name })
+	/* Metodo RAW do knex, para utilizar comando SQL. */
+	//await knex.raw("INSERT INTO courses (name) VALUES (?)", [name])
+
+	response.status(201).json()
+})
+
+app.get("/courses", async (request: Request, response: Response) => {
+	const courses = await knex("courses").select().orderBy("name")
+
+	response.status(200).json(courses)
+})
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+	const { id } = request.params
+	const { name } = request.body
+
+	await knex("courses").update({ name }).where({ id })
+
+	return response.json()
+})
+
+app.delete("/courses/:id", async (request: Request, response: Response) => {
+	const { id } = request.params
+
+	await knex("courses").delete().where({ id })
+
+	return response.json()
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
