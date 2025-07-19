8802937b6752cc9fc0354063ce6faa298b76304b
Kevin Solar <kevinleitesolar@hotmail.com>
2025-07-19 19:35:11 -0300

    feat: create the get route with join courses and modules.

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,22 @@
 export default {
-  client: "sqlite3",
-  connection: {
-    filename: "./src/database/database.db",
-  },
-  useNullAsDefault: true,
-  migrations: {
-    extension: "ts",
-    directory: "./src/database/migrations",
-  },
-  seeds: {
-    extension: "ts",
-    directory: "./src/database/seeds",
-  },
+	client: "sqlite3",
+	connection: {
+		filename: "./src/database/database.db",
+	},
+	pool: {
+    // define uma regra para usar a chave estrangeira.
+		afterCreate: (connection: any, done: any) => {
+			connection.run("PRAGMA foreign_keys = ON")
+			done()
+		},
+	},
+	useNullAsDefault: true,
+	migrations: {
+		extension: "ts",
+		directory: "./src/database/migrations",
+	},
+	seeds: {
+		extension: "ts",
+		directory: "./src/database/seeds",
+	},
 }
